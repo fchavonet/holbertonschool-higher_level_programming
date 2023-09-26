0f174c843244589d4267c7779a476a9cbfc54b34
fchavonet <fchavonet@mailo.com>
2023-09-26 20:33:34 +0200

    Fix: error message if args != 3

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
 from sys import argv
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
+
     if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     operator = argv[2]
