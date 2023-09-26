a5b5facc7ec53d8d45fa18c024374a2e5ea4fcc4
fchavonet <fchavonet@mailo.com>
2023-09-26 18:35:58 +0200

    Fix: put if/else in print to try

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    
-    if i % 2 == 0:
-        char_to_print = chr(i)
-    else:
-        char_to_print = chr(i - 32)
-
-    print(char_to_print, end='')
+    print(chr(i) if i % 2 == 0 else chr(i-32), end='')
