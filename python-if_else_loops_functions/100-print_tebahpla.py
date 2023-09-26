cb7a722b1ffe9ffb76a1191f99a9c9aa761d717a
fchavonet <fchavonet@mailo.com>
2023-09-26 19:06:02 +0200

    Fix: use two prints with format to test checker

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    print(chr(i) if i % 2 == 0 else chr(i-32), end='')
+
+    if i % 2 == 0:
+        print("{:c}".format(i), end='')
+    else:
+        print("{:c}".format(i - 32), end='')
