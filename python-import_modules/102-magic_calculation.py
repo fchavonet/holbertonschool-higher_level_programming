fa7f0147aef1127e1e4beaa903fdec74769427cb
fchavonet <fchavonet@mailo.com>
2023-09-29 09:00:05 +0200

    Fix: solve problem with output

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -2,10 +2,11 @@
 
 
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
     if a < b:
-        result = a + b
+        c = add(a, b)
         for i in range(4, 6):
-            result += i
-        return result
+            c = add(c, i)
+        return c
     else:
-        return a - b
+        return sub(a, b)
