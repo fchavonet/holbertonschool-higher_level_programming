7f27c26c2ac88eb9f645e7f90e7776ffa6edae03
fchavonet <fchavonet@mailo.com>
2023-10-02 21:24:33 +0200

    Fix: use del in place of append.

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -6,10 +6,8 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = []
-
     for number in range(len(my_list)):
-        if number != idx:
-            new_list.append(my_list[number])
+        if number == idx:
+            del my_list[number]
 
-    return new_list
+    return my_list
