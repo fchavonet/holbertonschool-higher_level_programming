f5cb5cd29697818854b8a45aed76d8bf455584d2
fchavonet <fchavonet@mailo.com>
2023-10-02 15:33:38 +0200

    Task 4. Replace in a copy.

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+
+    my_list_length = len(my_list)
+
+    if idx < 0 or idx > my_list_length - 1:
+        return (my_list)
+
+    new_list = my_list
+    new_list[idx] = element
+
+    return (new_list)
