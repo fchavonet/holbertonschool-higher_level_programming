2db9602c337ae41cee5b3b1bf51b382b940f5f3d
fchavonet <fchavonet@mailo.com>
2023-10-03 09:33:39 +0200

    Ta

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+
+    new_list = []
+    sum = 0
+
+    for number in my_list:
+        if number not in new_list:
+            new_list.append(number)
+            sum += number
+
+    return (sum)
