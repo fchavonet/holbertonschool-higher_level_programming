513be77eeafc052b0db150798f006c14ca27f6a0
fchavonet <fchavonet@mailo.com>
2023-10-04 18:13:43 +0200

    Fix: except error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,10 +8,9 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except Exception as e:
-            continue
-        else:
             elements_number += 1
+        except (TypeError, ValueError):
+            continue
     print()
 
     return elements_number
