7604164de8108d1653c7cbf634574ee18b29b164
fchavonet <fchavonet@mailo.com>
2023-10-09 09:12:21 +0200

    Add docstring.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
+"""
+This script defines a `Square` class to represent a square.
+"""
+
 class Square:
+    """
+    The "Square" class is used to represent a square.
+    """
     pass
