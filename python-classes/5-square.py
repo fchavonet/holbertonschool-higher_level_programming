0060bb73118e77c26e5b8d03811d4f409181de35
fchavonet <fchavonet@mailo.com>
2023-10-09 11:13:41 +0200

    Task 5. Printing a square.

---
+++ python-classes/5-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+"""
+This script defines a "Square" class to represent a square.
+"""
+
+
+class Square:
+    """
+    This is the Square class to represent a square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+                        It must be a non-negative integer.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+                         It must be a non-negative integer.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Prints in stdout the square with the character #
+        """
+        if not self.size:
+            print()
+        else:
+            for row in range(self.size):
+                for column in range(self.size):
+                    print("#", end="")
+                print()
