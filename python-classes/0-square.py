35e1e9b302d45191de501a54318ae1efe6236709
fchavonet <fchavonet@mailo.com>
2023-10-09 09:22:40 +0200

    Pycodestyle correcdtion.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 """
-This script defines a `Square` class to represent a square.
+This script defines a "Square" class to represent a square.
 """
 
+
 class Square:
-    """
-    The "Square" class is used to represent a square.
-    """
+    """The "Square" class is used to represent a square."""
     pass
