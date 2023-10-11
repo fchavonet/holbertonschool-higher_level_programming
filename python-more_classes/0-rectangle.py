0ff9999657c5a5da915bd474bf54caf046f7e52c
fchavonet <fchavonet@mailo.com>
2023-10-11 12:21:58 +0200

    Task 0. Simple rectangle.

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This script defines a `Rectangle` class to represent a square.
+"""
+
+
+class Rectangle:
+    """The `Rectangle` class is used to represent a square."""
+    pass
