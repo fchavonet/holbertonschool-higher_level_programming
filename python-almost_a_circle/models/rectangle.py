156c33b7d8f2dc94044fa65a74b71e9c3c54418c
fchavonet <fchavonet@mailo.com>
2023-10-23 12:26:12 +0200

    First upload of the task "5. Display #0".

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -193,3 +193,12 @@ class Rectangle(Base):
             int: the area of the rectangle.
         """
         return self.__width * self.__height
+
+    def display(self):
+        """
+        Prints in `stdout` the Rectangle instance with the character `#`.
+        """
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print("#", end="")
+            print()
