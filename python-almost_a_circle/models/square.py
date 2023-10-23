81d1edb4476b1b685ea5d0c6ca181fa80b9a85e8
fchavonet <fchavonet@mailo.com>
2023-10-23 19:48:26 +0200

    First upload of the task "11. Square size".

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -39,6 +39,37 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        Getter method to retrieve the size of the square's sides.
+
+        Returns:
+            int: the size of the square's sides.
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method to set the size of the square's sides.
+
+        Args:
+            value (int): the size of the square's sides.
+
+        Raises:
+            TypeError: if the value is not an integer.
+            ValueError: if the value is not greater than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """
         Return a string representation of the Square instance.
