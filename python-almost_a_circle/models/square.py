5df1db6878e27f1b43ad60bb9117cdabdbed5508
fchavonet <fchavonet@mailo.com>
2023-10-23 20:05:07 +0200

    First upload of the task "12. Square update".

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,15 +9,16 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    This is the Square class which is a specific type of rectangle
+    This is the `Square` class, which is a specific type of rectangle
     where all sides are of equal length.
 
-     Attributes:
+    Attributes:
         size (int): the length of each side of the square.
         x (int): the x-coordinate of the square (default is 0).
         y (int): the y-coordinate of the square (default is 0).
         id (int, optional): the unique identifier of
                             the square (default is None).
+
     Methods:
         __init__(self, size, x=0, y=0, id=None):
             Initializes a new Square instance.
@@ -25,12 +26,24 @@ class Square(Rectangle):
         __str__(self):
             Returns a string representation of the Square instance.
 
+        update(self, *args, **kwargs):
+            Assigns arguments to each attribute.
+
+        Args:
+            *args: a variable number of arguments.
+                The order is important:
+                    1st argument should be the `id` attribute
+                    2nd argument should be the `size` attribute
+                    3rd argument should be the `x` attribute
+                    4th argument should be the `y` attribute
+            **kwargs: a variable number of keyword arguments.
+                These allow you to update attributes by name.
     """
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initializes a new Square instance.
 
-        Attributes:
+        Args:
             size (int): the length of each side of the square.
             x (int): the x-coordinate of the square (default is 0).
             y (int): the y-coordinate of the square (default is 0).
@@ -62,10 +75,10 @@ class Square(Rectangle):
             ValueError: if the value is not greater than 0.
         """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("size must be an integer")
 
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("size must be > 0")
 
         self.width = value
         self.height = value
@@ -74,10 +87,43 @@ class Square(Rectangle):
         """
         Return a string representation of the Square instance.
 
-        Return:
-            str: a string representing the Square instance in the format
-            "([Rectangle] ({}) {}/{} - {}"
-             .format(self.id, self.x, self.y, self.width)).
+        Returns:
+            str: A string representing the Square instance in the format
+            "[Square] ({}) {}/{} - {}"
+            .format(self.id, self.x, self.y, self.size).
         """
         return ("[Square] ({}) {}/{} - {}"
-                .format(self.id, self.x, self.y, self.width))
+                .format(self.id, self.x, self.y, self.size))
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns arguments to each attribute.
+
+        Args:
+            *args: a variable number of arguments.
+                The order is important:
+                    1st argument should be the `id` attribute
+                    2nd argument should be the `size` attribute
+                    3rd argument should be the `x` attribute
+                    4th argument should be the `y` attribute
+            **kwargs: a variable number of keyword arguments.
+                These allow you to update attributes by name.
+        """
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
