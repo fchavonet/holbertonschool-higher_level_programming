c5152741a5a315665f4edf9b60b691cf6c914244
fchavonet <fchavonet@mailo.com>
2023-10-24 08:48:03 +0200

    Fix: raise error messages.

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -75,10 +75,10 @@ class Square(Rectangle):
             ValueError: if the value is not greater than 0.
         """
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
 
         if value <= 0:
-            raise ValueError("size must be > 0")
+            raise ValueError("width must be > 0")
 
         self.width = value
         self.height = value
