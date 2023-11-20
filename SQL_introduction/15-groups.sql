2e730087e1eaad0096a74657e277bfeef2bb1bd2
fchavonet <fchavonet@mailo.com>
2023-11-20 12:16:29 +0100

    First upload of task "15. Number by score".

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+--  List the number of records with the same score in the table `second_table` of the database `hbtn_0c_0` in the MySQL server.
+SELECT score,
+COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY NUMBER DESC;
