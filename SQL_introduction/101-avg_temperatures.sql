473d27790b1b876978f547f3c92c1f05fd1e3c30
fchavonet <fchavonet@mailo.com>
2023-11-20 13:25:28 +0100

    Fix: change average to avg_temp name.

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Display the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SELECT city, AVG(value) AS average
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY average DESC;
+ORDER BY avg_temp DESC;
