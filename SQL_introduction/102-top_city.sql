499ffb0af225bf44ef4393beb0c80e112ef07d08
fchavonet <fchavonet@mailo.com>
2023-11-20 13:36:19 +0100

    Fix: description comment.

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
--- Display the max temperature of each state (ordered by State name).
+-- Displays the top 3 of cities temperature during July and August ordered by temperature (descending).
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
