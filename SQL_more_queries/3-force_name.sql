b507b94d939c4d02db2d6f55f8a9cc0680c9387f
fchavonet <fchavonet@mailo.com>
2023-11-21 08:20:32 +0100

    Fix: error in table name.

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create the table `force_name` on the MySQL server.
-CREATE TABLE IF NOT EXISTS force_table (
+CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
