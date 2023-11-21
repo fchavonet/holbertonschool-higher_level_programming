36f6292a4b05a17d4edcfdc6743cfe971645987b
fchavonet <fchavonet@mailo.com>
2023-11-21 11:19:29 +0100

    Fix: reverse elements order.

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- List all shows contained in `hbtn_0d_tvshows` that have at least one genre linked.
-SELECT tv_show_genres.genre_id, tv_shows.title FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 JOIN  tv_shows_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_show_genres.genre_id, tv_shows.title;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
