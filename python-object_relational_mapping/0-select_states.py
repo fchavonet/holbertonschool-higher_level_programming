a3fad34d0332471200e0d7cff974427d0c9e1c45
fchavonet <fchavonet@mailo.com>
2023-11-22 12:12:34 +0100

    First upload of task "0. Get all states".

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module that list all states from a database.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host = "localhost",
+        port = 3306,
+        user = sys.argv[1],
+        passwd = sys.argv[2],
+        database = sys.argv[3]
+    )
+
+    cursor = database.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    database.close()
