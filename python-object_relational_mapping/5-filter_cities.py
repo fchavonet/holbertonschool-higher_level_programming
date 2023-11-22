c71f6f4389e5ff435868ff52d7bdaa4bf99f0972
fchavonet <fchavonet@mailo.com>
2023-11-22 17:43:03 +0100

    First upload of task "5. All cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module that list all cities of a state from a database.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    cursor = database.cursor()
+
+    cursor.execute("""
+                   SELECT cities.name FROM cities
+                   JOIN states ON cities.state_id = states.id
+                   WHERE states.name LIKE BINARY %s
+                   ORDER BY cities.id
+                   """, (sys.argv[4],))
+
+    results = cursor.fetchall()
+
+    if results:
+        print(", ".join(row[0] for row in results))
+
+    cursor.close()
+    database.close()
