5fe64dfba9756e8b59f6a3a41eedaa6adeaa5193
fchavonet <fchavonet@mailo.com>
2023-11-23 11:13:45 +0100

    Fix: chmod...

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,13 +6,11 @@ Modul that lists all State objects from the database `hbtn_0e_6_usa`.
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sys import argv
+import sys
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(argv[1], argv[2], argv[3]))
-
-    Base.metadata.create_all(engine)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Session = sessionmaker(bind=engine)
     session = Session()
