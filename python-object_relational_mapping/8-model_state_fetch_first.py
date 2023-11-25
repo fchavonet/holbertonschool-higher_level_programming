0f1034d4a3a6b2a5c7620ed9f25c73016d5545bc
fchavonet <fchavonet@mailo.com>
2023-11-25 18:00:08 +0100

    Fix: add s to state.id.

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     states = session.query(State).order_by(State.id).first()
 
     if states:
-        print("{}: {}".format(state.id, state.name))
+        print("{}: {}".format(states.id, states.name))
     else:
         print("Nothing")
 
